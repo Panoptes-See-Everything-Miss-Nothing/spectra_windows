6d3e1128121809a7c285e4c0068256ea6a849d72
SlidingWindow <sliding.window0xff@gmail.com>
2026-02-26 18:39:45 +1100

    Rename WindowsEnum to spectra-windows-sensor

--- WindowsEnum/src/RegistryUtils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "./Utils/Utils.h"
-#include <winreg.h>
-
-// Read REG_SZ or REG_EXPAND_SZ registry value safely
-std::wstring GetRegistryString(HKEY hKey, const std::wstring& valueName);
